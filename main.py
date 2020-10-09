4ba566c09e1182a05b616de4505e859e1337bdd0
Joe Clinton <48254978+Joeclinton1@users.noreply.github.com>
2020-10-09 22:19:53 +0100

    Change name of module parents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from Scrible_bot.settings import Settings
-from Scrible_bot.skribbl_bot import SkribblBot
-from Scrible_bot.tkinter_gui import TkinterGui
+from Skribbl-Bot.settings import Settings
+from Skribbl-Bot.skribbl_bot import SkribblBot
+from Skribbl-Bott.tkinter_gui import TkinterGui
 
 if __name__ == '__main__':
     settings = Settings()
